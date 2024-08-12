127f3f020ef9bba5d18d71a5217af3069966fe30
Fabiana Barros <fabiana.barros@paydock.com>
2024-08-12 18:25:02 +0100

    fix(SDK-595): Fixed widget not reloading when there's an error on payment

--- assets/build/js/frontend/pay-pal-wallet.asset.php
+++ assets/build/js/frontend/pay-pal-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'b4b0e4202234bd49d27f');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '5ac53f4751dcfd7c23f4');
