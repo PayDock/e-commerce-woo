86a1d3e8672a2ffbc1f5117ff414fc25ca1c3b05
FabianaRBarros <53559035+FabianaRBarros@users.noreply.github.com>
2024-08-13 12:26:13 +0100

    Fix SDK-595 (#16)
    * fix(SDK-595): Fixed widget not reloading when there's an error on payment
    
    * fix(SDK-595): Fixed widget not reloading when there's an error on payment - minor fix
    
    ---------
    
    Co-authored-by: Fabiana Barros <fabiana.barros@paydock.com>

--- assets/build/js/frontend/apple-pay-wallet.asset.php
+++ assets/build/js/frontend/apple-pay-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'badcce6a34b7a7dd2bfd');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'b686f79f7e891e460956');
--- assets/build/js/frontend/google-pay-wallet.asset.php
+++ assets/build/js/frontend/google-pay-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '06be44d26930196049fc');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '2be687751b42435df0ae');
