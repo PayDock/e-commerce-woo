be7249e2777fdf680cb7b12d23104fd11ac3aafb
Danyl Kushnarov <danyl.kushnaryov@paydock.com>
2024-08-21 15:26:17 +0200

    SDK-676 build assets

--- assets/build/js/frontend/apple-pay-wallet.asset.php
+++ assets/build/js/frontend/apple-pay-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'c58c6870324ab9f4b26e');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'cd51a868fee669126986');
