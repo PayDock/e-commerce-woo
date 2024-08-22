e198c30b66458e7db8e7b676484c7163c8ea91ae
Danyl Kushnarov <danyl.kushnaryov@paydock.com>
2024-08-22 13:42:41 +0200

    SDK-682 build assets

--- assets/build/js/frontend/afterpay-a-p-m-s.asset.php
+++ assets/build/js/frontend/afterpay-a-p-m-s.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '7a0480fe22fe611b180d');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'a0e0fe2e10a8217c891e');
