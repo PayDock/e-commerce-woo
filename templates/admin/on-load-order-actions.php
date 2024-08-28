80c986209813dc459cd92815fff9169349899b0e
Fabiana Barros <fabiana.barros@paydock.com>
2024-08-28 15:08:38 +0100

    fix(SDK-698): Fix select box not updating

--- templates/admin/on-load-order-actions.php
+++ templates/admin/on-load-order-actions.php
@@ -1,5 +1,8 @@
 <script>
   jQuery( function($) {
-    $('select[name="order_status"]').prop('autocomplete', 'off');
+    $( window ).on( "beforeunload", function() {
+      $('.form-field.wc-order-status').remove()
+      console.log("Handler for `beforeunload` called.")
+    } );
   } )
 </script>
