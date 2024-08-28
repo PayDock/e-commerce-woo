dfe871fa5eb89cfe68317ce783598a18c684aa1e
Danyl-Kushnaryov <danyl.kushnaryov@paydock.com>
2024-08-28 20:00:45 +0200

    SKD-700 Unable to raise sale orders via cards for a user that is not logged in to WC (#56)
    * SDK-700 fix powerboardCCFormValidation fn
    
    * SDK-700 build assets
    
    * SDK-700 build assets

--- assets/js/frontend/form.js
+++ assets/js/frontend/form.js
@@ -32,7 +32,7 @@ setTimeout(() => jQuery(function ($) {
         lastWcFormValidation: false,
         powerboardCCFormValidation() {
             const { tokens, selectedToken } = window.wc.wcSettings.getSetting('power_board_data', {});
-            return (tokens.length > 0 && selectedToken !== "") || window.widgetPowerBoard.isValidForm();
+            return (Array.isArray(tokens) && tokens.length > 0 && selectedToken !== "") || window.widgetPowerBoard.isValidForm();
         },
         wcFormValidation() {
             const checkoutFormElements = document.querySelectorAll('.wc-block-checkout__form input, .wc-block-checkout__form select');
