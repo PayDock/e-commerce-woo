fd63b605ab0fc7f14176ee0cd28570e468f4aa9b
Fabiana Barros <fabiana.barros@paydock.com>
2024-08-30 11:02:15 +0100

    Updated build files

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'ec5766c860a3abd0ea05');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '66b86b0f152bb7126969');
