8e1e8e2249bddbb4998e057d02bd382bb6500bce
Danyl-Kushnaryov <danyl.kushnaryov@paydock.com>
2024-09-03 18:18:29 +0200

    SDK-706 ZipPay- Afterpay v1 - When updating the cart total, the payment is failing (#66)
    * SDK-706 fix the total amount when updating the cart total
    
    * SDK-706 build assets

--- assets/build/js/frontend/afterpay-a-p-m-s.asset.php
+++ assets/build/js/frontend/afterpay-a-p-m-s.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '06ecb9c79e8aa1a084bf');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '9b315a6076e4611c7575');
