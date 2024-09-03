a185693e84baafcee2effbec3c7f0d459031c98e
Danyl Kushnarov <danyl.kushnaryov@paydock.com>
2024-09-03 15:51:24 +0200

    SDK-706 build assets

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '66b86b0f152bb7126969');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'c31cbc6c4017e6031069');
