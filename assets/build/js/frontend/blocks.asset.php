128d051b3a6b455c1bdc7042e19fc63592bcebf4
Fabiana Barros <fabiana.barros@paydock.com>
2024-09-12 15:41:32 +0100

    Minor fixes

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'fe1fe6cf4b15aad22781');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'a903d192ba2815c56ffe');
--- includes/Helpers/VaultTokenHelper.php
+++ includes/Helpers/VaultTokenHelper.php
@@ -59,7 +59,7 @@ class VaultTokenHelper {
 				$parsed_api_error = __( 'Unable to create PowerBoard vault token', 'power-board' );
 			}
 
-			$parsed_api_error .= ' <input id="widget_error" hidden type="text"/>';
+			$parsed_api_error .= ' widget_error';
 
 			throw new Exception( esc_html( $parsed_api_error ) );
 
