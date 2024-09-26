ddc187206a0135cc565fd5d77c255bcedc7c09e8
Danyl Kushnarov <danyl.kushnaryov@paydock.com>
2024-09-26 18:08:48 +0200

    SDK-755 build assets

--- assets/build/js/frontend/afterpay-wallet.asset.php
+++ assets/build/js/frontend/afterpay-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'fe3106530778d8889d4f');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '5cfebc3753b282bf6580');
