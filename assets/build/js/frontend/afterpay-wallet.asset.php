9da1914cbb98cd6cd76a2877201933755e4be4cb
Fabiana Barros <fabiana.barros@paydock.com>
2024-10-01 09:14:17 +0100

    Updated build files

--- assets/build/js/frontend/afterpay-wallet.asset.php
+++ assets/build/js/frontend/afterpay-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '7d696cf290363a20c63d');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '2d9a4fe5e9db16c5c854');
--- assets/build/js/frontend/pay-pal-wallet.asset.php
+++ assets/build/js/frontend/pay-pal-wallet.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'ae1ca89600a3527ba764');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'b5454d51d286d3d5f74e');
