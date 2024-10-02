4f91719730bd62d889c25379d0a471d1eb8161c5
Danyl-Kushnaryov <danyl.kushnaryov@paydock.com>
2024-10-02 13:39:18 +0200

    SDK-757 fix incorrect dependency defitions (#91)
    * SDK-757 fix incorrect dependency defitions
    
    * SDK-757 remove package-lock.json from the .gitignore
    
    * SDK-757 fix vulnerabilities for dependencies, stick to the exact versions in the dev dependencies
    
    * SDK-757 build assets
    
    * Revert " SDK-757 remove package-lock.json from the .gitignore"
    
    This reverts commit 549aef90
    
    * Updated build files
    
    ---------
    
    Co-authored-by: Fabiana Barros <fabiana.barros@paydock.com>

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => '552e626d24a8014266cc');
+<?php return array('dependencies' => array('react', 'react-dom', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'a8748b373607bafc77ce');
