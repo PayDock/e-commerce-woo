1ef1475bba3d8cb424f157e05f2fdb07845289d9
andryes <nord.vc@gmail.com>
2024-11-06 15:28:46 +0200

    Remove unused files from includes (SDK-904)

--- power-board.php
+++ power-board.php
@@ -42,7 +42,6 @@ if ( ! defined( 'POWER_BOARD_PLUGIN_VERSION' ) ) {
 	define( 'POWER_BOARD_PLUGIN_VERSION', '3.1.3' );
 }
 
-require_once __DIR__ . '/includes/phpseclib/bootstrap.php';
-require_once 'vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 PowerBoard\PowerBoardPlugin::getInstance();
