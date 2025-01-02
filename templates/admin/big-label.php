efbaba5f7ab799282db418d9c1b02a1a220142b8
FabianaBarros <fabiana.barros@paydock.com>
2025-01-02 11:54:59 +0000

    ECOMM-40 | Removed unnecessary obsolete tags

--- templates/admin/big-label.php
+++ templates/admin/big-label.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly ?>
-<tr valign="top">
+<tr>
 	<th scope="row" class="titledesc">
 		<h1><?php echo wp_kses_post( $value['title'] ); ?></h1>
 	</th>
--- templates/admin/label.php
+++ templates/admin/label.php
@@ -1,7 +1,7 @@
 <?php if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } // Exit if accessed directly ?>
-<tr valign="top">
+<tr>
 	<th scope="row" class="titledesc">
 		<h3><?php echo wp_kses_post( $value['title'] ); ?></h3>
 	</th>
