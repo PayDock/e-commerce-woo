d3359ff35339c0e9d60c0b703b5211383b70ab70
Robert van Vark <robert.van.vark@paydock.com>
2025-01-09 15:42:37 +0100

    ECOMM-23: uses script to remove files to be excluded from zip so it can be reused

---
+++ cleanup_files_for_zip.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+rm -fR .git
+rm .gitignore
+rm -fR .github
+rm .gitlab-ci.yml
+rm .codeclimate.yml
+rm -fR .idea
+rm screenshot-*.png
+rm phpstan.neon
+rm phpcs2codeclimate.py
+rm phpcs.xml
+rm eslint.config.mjs
+rm cleanup_files_for_zip.sh
