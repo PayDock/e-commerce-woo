682a3439a4756a1742021a4c73c60e5d6f51bcaa
Fabiana Barros <fabiana.barros@paydock.com>
2025-01-29 13:05:08 +0000

    fix: Fixed create intent not working when order id is not a string

--- includes/Controllers/Admin/WidgetController.php
+++ includes/Controllers/Admin/WidgetController.php
@@ -74,7 +74,7 @@ class WidgetController {
 			'amount'        => round( $request['total']['total_price'] / 100, 2 ),
 			'version'       => (int) $settings->get_checkout_template_version(),
 			'currency'      => $request['total']['currency_code'],
-			'reference'     => $reference,
+			'reference'     => (string) $reference,
 			'customer'      => [
 				'email'           => $billing_address['email'],
 				'billing_address' => [
