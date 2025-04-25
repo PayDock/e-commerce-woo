0a46184ac7d194ea7da1777fcf00b989f039ca7b
Andrii Yes <andrii.yeshchenko@paydock.com>
2025-04-25 14:16:41 +0300

    Fix: PB build, correct dir and plugin key (to prevent wrong directory notice)

--- build/power-board/config-powerboard.php
+++ build/power-board/config-powerboard.php
@@ -1,12 +1,12 @@
 <?php
 return [
 	'PLUGIN_NAME'                    => 'PowerBoard for WooCommerce',
-	'PLUGIN_NAME_KEY'                => 'powerboard-for-woocommerce',
+	'PLUGIN_NAME_KEY'                => 'power-board',
 	'PLUGIN_URI'                     => 'https://github.com/CommBank-PowerBoard/powerboard-e-commerce-woo',
 	'PLUGIN_DESCRIPTION'             => 'PowerBoard simplify how you manage your payments. Reduce costs, technical headaches & streamline compliance using PowerBoard\'s payment orchestration.',
 	'PLUGIN_AUTHOR'                  => 'PowerBoard',
 	'PLUGIN_AUTHOR_URI'              => 'https://www.commbank.com.au/business/payments/take-online-payments/powerboard.html#getting-started',
-	'PLUGIN_VERSION'                 => '1.0.0',
+	'PLUGIN_VERSION'                 => '1.4.0',
 	'PLUGIN_WIDGET_NAME'             => 'cba',
 	'PLUGIN_TEXT_DOMAIN'             => 'power-board',
 	'PLUGIN_PREFIX'                  => 'power_board',
