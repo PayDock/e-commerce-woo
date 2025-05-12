2c417ec55e43f273ac8feba5caac4f4b4681fe05
jpcostello <john.costello@paydock.com>
2025-05-12 13:30:48 +0100

    Rerun build

--- assets/build/js/frontend/blocks.asset.php
+++ assets/build/js/frontend/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'eb71ba9fe6110aa26c8d');
+<?php return array('dependencies' => array('react', 'wc-blocks-data-store', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-html-entities', 'wp-i18n'), 'version' => 'dab05016b31acdbaa83d');
