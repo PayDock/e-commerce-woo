ba82afde3c694086b988bab0ead11dc06bf9b2aa
Andrii Yeshchenko <andrii.yeshchenko@paydock.com>
2025-10-30 19:48:35 +0200

    Delete node_modules

--- cleanup_files_for_zip.sh
+++ cleanup_files_for_zip.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
 rm -fR .git
-rm .gitignore
+rm -f .gitignore
 rm -fR .github
-rm .gitlab-ci.yml
-rm .codeclimate.yml
+rm -f .gitlab-ci.yml
+rm -f .codeclimate.yml
+rm -f CODEOWNERS
 rm -fR .idea
-rm screenshot-*.png
-rm phpstan.neon
-rm phpcs2codeclimate.py
-rm phpcs.xml
-rm eslint.config.mjs
-rm VERSION.TXT
-rm -r build
-rm -r tests
-rm cleanup_files_for_zip.sh
+rm -fR .vscode
+rm -f phpstan.neon
+rm -f phpcs2codeclimate.py
+rm -f phpcs.xml
+rm -f eslint.config.mjs
+rm -f screenshot-*.png
+rm -fR build
+rm -fR tests
+rm -f VERSION.TXT
+rm -fR node_modules
+rm -f cleanup_files_for_zip.sh
